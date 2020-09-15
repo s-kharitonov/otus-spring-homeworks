0dfaa7fb7d04790cf73069e910e1e3da5f380195
Sergey Kharitonov <skharitonov95@gmail.com>
2020-09-15 19:08:38 +0300

    remove useless config class

--- hw2-spring-annotation-configuration/src/main/java/ru/otus/Main.java
+++ hw2-spring-annotation-configuration/src/main/java/ru/otus/Main.java
@@ -1,13 +1,16 @@
 package ru.otus;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.otus.configs.AppConfig;
+import org.springframework.context.annotation.*;
 import ru.otus.runners.Runner;
 
+@ComponentScan
+@PropertySource("classpath:application.properties")
+@EnableAspectJAutoProxy
+@Configuration
 public class Main {
 
 	public static void main(String[] args) {
-		final var context = new AnnotationConfigApplicationContext(AppConfig.class);
+		final var context = new AnnotationConfigApplicationContext(Main.class);
 		final var runner = context.getBean(Runner.class);
 
 		runner.run();
