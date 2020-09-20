83251eb550f9566788964e3dd34813992a021741
Sergey Kharitonov <skharitonov95@gmail.com>
2020-09-20 18:26:04 +0300

    init new module

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,4 +1,5 @@
 public final class Versions {
 	public static final String openCSV = "5.2";
 	public static final String aspectJ = "1.9.6";
+	public static final String shell = "2.0.1.RELEASE";
 }
---
+++ hw5-books-jdbc/src/main/java/ru/otus/Main.java
@@ -0,0 +1,11 @@
+package ru.otus;
+
+import org.springframework.context.annotation.*;
+import ru.otus.services.TestService;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
