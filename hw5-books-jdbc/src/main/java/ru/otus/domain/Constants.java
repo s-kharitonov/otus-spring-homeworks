12182f628cd59449e17f98dec14223d7cd01331f
s-kharitonov <skharitonov95@gmail.com>
2020-09-29 23:55:35 +0300

    added authors controller

--- hw5-books-jdbc/src/main/java/ru/otus/domain/Constants.java
+++ hw5-books-jdbc/src/main/java/ru/otus/domain/Constants.java
@@ -19,5 +19,12 @@ public final class Constants {
 	public static final String GENRE_UNSUCCESSFUL_UPDATED_MSG_KEY = "genre.unsuccessful.updated";
 	public static final String GENRE_NOT_FOUND_MSG_KEY = "genre.not.found";
 
+	public static final String AUTHOR_SUCCESSFUL_REMOVED_MSG_KEY = "author.successful.removed";
+	public static final String AUTHOR_UNSUCCESSFUL_REMOVED_MSG_KEY = "author.unsuccessful.removed";
+	public static final String AUTHOR_UNSUCCESSFUL_CREATED_MSG_KEY = "author.unsuccessful.created";
+	public static final String AUTHOR_SUCCESSFUL_UPDATED_MSG_KEY = "author.successful.updated";
+	public static final String AUTHOR_UNSUCCESSFUL_UPDATED_MSG_KEY = "author.unsuccessful.updated";
+	public static final String AUTHOR_NOT_FOUND_MSG_KEY = "author.not.found";
+
 	public static final String DEFAULT_DATE_PATTERN = "dd.MM.yyyy";
 }
