982c9dc5cb7ed43676c268d609eed8b7488b5a1d
s-kharitonov <skharitonov.k@gmail.com>
2020-10-11 19:38:05 +0300

    resolve N+1 problem

--- hw7-books-spring-data-jpa/src/main/java/ru/otus/repositories/BooksRepository.java
+++ hw7-books-spring-data-jpa/src/main/java/ru/otus/repositories/BooksRepository.java
@@ -1,8 +1,14 @@
 package ru.otus.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import ru.otus.domain.Book;
 
-public interface BooksRepository extends JpaRepository<Book, Long> {
+import java.util.List;
 
+public interface BooksRepository extends JpaRepository<Book, Long> {
+	@NonNull
+	@Query(name = "book.find.all")
+	List<Book> findAll();
 }
---
+++ hw7-books-spring-data-jpa/src/test/java/ru/otus/repositories/BooksRepositoryUnitTest.java
@@ -0,0 +1,32 @@
+package ru.otus.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.otus.domain.Book;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@DisplayName("repository for work with books")
+class BooksRepositoryUnitTest {
+
+	@Autowired
+	private BooksRepository booksRepository;
+
+	@Autowired
+	private TestEntityManager em;
+
+	@Test
+	@DisplayName("should return all books")
+	public void shouldReturnAllBooks() {
+		var books = booksRepository.findAll();
+		var expectedBooks = em.getEntityManager()
+				.createQuery("select b from Book b", Book.class)
+				.getResultList();
+
+		assertThat(books).containsOnlyOnceElementsOf(expectedBooks);
+	}
+}
