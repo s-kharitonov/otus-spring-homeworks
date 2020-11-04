341900c845c7c1520ade46d91d139caef861db49
s-kharitonov <skharitonov.k@gmail.com>
2020-11-04 11:34:36 +0300

    change date pattern

--- hw9-books-MVC/src/main/java/ru/otus/domain/Constants.java
+++ hw9-books-MVC/src/main/java/ru/otus/domain/Constants.java
@@ -1,5 +1,5 @@
 package ru.otus.domain;
 
 public final class Constants {
-	public static final String DEFAULT_DATE_PATTERN = "YYYY-MM-DD";
+	public static final String DEFAULT_DATE_PATTERN = "YYYY-MM-dd";
 }
