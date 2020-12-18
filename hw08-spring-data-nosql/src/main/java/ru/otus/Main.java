d67ae7902f84375890c2a218f6528ea82b602d6d
s-kharitonov <skharitonov.k@gmail.com>
2020-12-18 19:48:45 +0300

    delete useless properties

--- hw08-spring-data-nosql/src/main/java/ru/otus/Main.java
+++ hw08-spring-data-nosql/src/main/java/ru/otus/Main.java
@@ -3,13 +3,10 @@ package ru.otus;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import ru.otus.configs.AppProperties;
 
 @EnableMongock
 @EnableMongoRepositories
-@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class Main {
 
