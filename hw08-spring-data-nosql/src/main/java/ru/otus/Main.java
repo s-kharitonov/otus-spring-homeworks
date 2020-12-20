709cf3dcc5497b8905157eb6d92ae6ca02d9941f
s-kharitonov <skharitonov.k@gmail.com>
2020-12-20 16:39:32 +0300

    enable app configs

--- hw08-spring-data-nosql/src/main/java/ru/otus/Main.java
+++ hw08-spring-data-nosql/src/main/java/ru/otus/Main.java
@@ -3,11 +3,13 @@ package ru.otus;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongock
 @EnableMongoRepositories
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Main {
 
 	public static void main(String[] args) {
