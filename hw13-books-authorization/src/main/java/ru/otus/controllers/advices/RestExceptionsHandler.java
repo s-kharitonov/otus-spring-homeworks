a4c999d77e939702851862dfa88229f42a5a3498
s-kharitonov <skharitonov.k@gmail.com>
2021-01-16 20:01:46 +0300

    added exception handler for AccessDeniedException

--- hw13-books-authorization/src/main/java/ru/otus/controllers/advices/RestExceptionsHandler.java
+++ hw13-books-authorization/src/main/java/ru/otus/controllers/advices/RestExceptionsHandler.java
@@ -1,18 +1,27 @@
 package ru.otus.controllers.advices;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.otus.domain.ErrorResponse;
 import ru.otus.exceptions.NotFoundException;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class RestExceptionsHandler {
 
 	@ExceptionHandler(NotFoundException.class)
 	private ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+		return ResponseEntity.status(NOT_FOUND)
+				.body(new ErrorResponse(NOT_FOUND.value(), e.getMessage()));
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	private ResponseEntity<ErrorResponse> handleAccessDenied(AccessDeniedException e) {
+		return ResponseEntity.status(FORBIDDEN)
+				.body(new ErrorResponse(FORBIDDEN.value(), e.getMessage()));
 	}
 }
